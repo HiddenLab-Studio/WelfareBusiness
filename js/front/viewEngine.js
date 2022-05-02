f3dd97d5ec22af14ba11ae5ba5fdbd86b038f0e4
AurelienRoge <aurelien.roge@student.yncrea.fr>
2022-05-02 10:01:34 +0200

    init

--- js/front/viewEngine.js
+++ js/front/viewEngine.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 563,
     scene: {
         preload: preload,
         create: create,
@@ -12,11 +12,11 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload (){
-
+    this.load.image('theo', 'img/a-theo_emote.png/')
 }
 
 function create (){
-
+    this.add.image(400, 250, 'theo');
 }
 
 function update (){

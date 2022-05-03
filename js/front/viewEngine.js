23fe5e8965372c52cee0a908c447112895d2bfee
AurelienRoge <aurelien.roge@student.yncrea.fr>
2022-05-03 11:05:07 +0200

    barre de progression

--- js/front/viewEngine.js
+++ js/front/viewEngine.js
@@ -2,7 +2,7 @@ var config = {
     type: Phaser.AUTO,
     width: 1000,
     height: 563,
-    backgroundColor : '#DAD1CF',
+    backgroundColor: '#DAD1CF',
     scene: {
         preload: preload,
         create: create,
@@ -28,28 +28,21 @@ function create() {
     let topbarHeight = 71;
 
 
-
-
-    //progressbar
-    let progressbarStyle = this.make.graphics().fillStyle(0x00ff00).fillRect(0, 0, config.width, 10);
-    progressbarStyle.generateTexture('progressbar', config.width, 10);
-    let progressbar = this.add.image(0, 0, 'progressbar').setOrigin(0, 0);
-
     //avatarDisplay
-    let avatardisplayStyle = this.make.graphics().fillStyle(0x0000ff).fillRect(0, 0, 1.5*topbarHeight, 1.5*topbarHeight);
-    avatardisplayStyle.generateTexture('avatarDisplay', 1.5*topbarHeight, 1.5*topbarHeight);
+    let avatardisplayStyle = this.make.graphics().fillStyle(0x0000ff).fillRect(0, 0, 1.5 * topbarHeight, 1.5 * topbarHeight);
+    avatardisplayStyle.generateTexture('avatarDisplay', 1.5 * topbarHeight, 1.5 * topbarHeight);
     let avatarDisplay = this.add.image(30, 30, 'avatarDisplay').setOrigin(0, 0);
 
     //Barre d'info (argent, bonheur, date et vitesse de jeu)
-    let infobarStyle = this.make.graphics().fillStyle(0x00ffff).fillRect(0, 0, 4 * topbarHeight, 1.2*topbarHeight);
-    infobarStyle.generateTexture('infobar', 4 * topbarHeight, 1.2*topbarHeight);
+    let infobarStyle = this.make.graphics().fillStyle(0x00ffff).fillRect(0, 0, 4 * topbarHeight, 1.2 * topbarHeight);
+    infobarStyle.generateTexture('infobar', 4 * topbarHeight, 1.2 * topbarHeight);
     let infobar = this.add.image(136, 40, 'infobar').setOrigin(0, 0);
 
     let moneyIcon = this.add.image(150, 50, 'theo').setScale(0.04).setOrigin(0, 0).setInteractive();
     let moneyString = "Valeur d'argent";
     let moneyText = this.add.text(170, 50, moneyString, { font: "14px Arial", fill: "#000000" });
 
-    
+
     let happinessIcon = this.add.image(150, 70, 'theo').setScale(0.04).setOrigin(0, 0).setInteractive();
     let happinessString = "Valeur de bonheur";
     let happinessText = this.add.text(170, 70, happinessString, { font: "14px Arial", fill: "#000000" });
@@ -80,5 +73,18 @@ function create() {
 }
 
 function update() {
+    //Fonction d'affichage de la barre de progression en fonction du poucentage de progression
+    function displayProgressBar(game, percent) {
+        let progressbarStyle = game.make.graphics().fillStyle(0x00ff00).fillRect(0, 0, percent * config.width, 10);
+        progressbarStyle.generateTexture('progressbar', percent * config.width, 10);
+
+        let progressbar = game.add.image(0, 0, 'progressbar').setOrigin(0, 0);
+        progressbar.setDisplaySize(percent, 10);
+
+        return progressbar;
+    }
+
+
+    let progressbar = displayProgressBar(this, i);
 
 }

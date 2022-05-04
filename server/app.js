daaa4d1abb7b6e5f71fb9b47965557c4eca23767
zZeTraL <lucashbs.pro@gmail.com>
2022-05-04 16:49:33 +0200

    🌠 Update - Login system is now working - Register system is now working

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // Déclaration des modules importants
 const express = require("express");
-const http = require("http")
+const http = require("http");
+const mysql = require("mysql");
 
 // Déclaration des modules utiles
 const app = express();
@@ -8,6 +9,7 @@ const path = require("path");
 const compression = require("compression");
 const dotenv = require("dotenv").config({path: "./.env"});
 const bodyParser = require("body-parser");
+const flash = require("express-flash");
 
 // Configuration de notre session
 const session = require('express-session')({
@@ -17,10 +19,21 @@ const session = require('express-session')({
     saveUninitialized: true
 });
 
+// Configuration de la base de données
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+})
+
 // Configuration du serveur
 app.use(express.static(path.join(__dirname, "..")));
 app.use(compression());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(session)
+app.use(flash());
 app.set("title", process.env.SERVER_NAME)
 app.set("port", process.env.SERVER_PORT);
 app.set("env", process.env.SERVER_ENV)
@@ -29,11 +42,42 @@ if(app.get("env") === "production"){
     app.set("trust proxy", 1);
 }
 
+// Variable globales
+global.flash = flash;
+global.pool = pool;
+
 // Home page
 app.get('/', (req, res) => {
-    res.render(path.join(__dirname, "..", "views", "index"));
+    // Si l'utilisateur n'est pas connecté
+    if(req.session.login){
+        // On fait le rendu de notre page en enlevant le bouton de connexion
+        res.render(path.join(__dirname, "..", "views", "index"), {
+            session: true,
+            username: req.session.username,
+        });
+    } else {
+        // Si la session du joueur n'est pas défini on le met offline
+        if(req.session.login === undefined) req.session.login = false;
+        // Flash message pour afficher si une erreur est détecté pour le login ou le register
+        const registerError = req.flash("registerError");
+        const registerUsernameError = req.flash("registerUsernameError");
+        const registerPasswordError = req.flash("registerPasswordError");
+        const registerConfirmError = req.flash("registerConfirmError");
+        const registerUsernameAlreadyUsed = req.flash("registerUsernameAlreadyUsed");
+        const loginError = req.flash("loginError");
+        const loginUsernameError = req.flash("loginUsernameError");
+        const loginPasswordError = req.flash("loginPasswordError");
+        // Rendu de la page avec le bouton pour se connecter
+        res.render(path.join(__dirname, "..", "views", "index"), {
+            session: false,
+            login: {error: loginError[0], username: loginUsernameError[0], password: loginPasswordError[0]},
+            register: {error: registerError[0], username: registerUsernameError[0], password: registerPasswordError[0], confirm: registerConfirmError[0], user: registerUsernameAlreadyUsed[0]},
+        });
+
+    }
 })
 
+// Controllers
 const auth = require("./controllers/authController");
 app.use("/", auth);
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,12 +28,183 @@ async function compareHash(password, hashed) {
     return await bcrypt.compareSync(password, hashed);
 }
 
+/**
+ * Permet de check si un mot de passe rempli nos conditions fixées
+ *
+ * @param password
+ * @returns {boolean}
+ */
+function isPasswordValid(password) {
+    // Si notre passe fait moins de 6 caractère OU notre passe contient des espaces
+    if (password.length < 2 || password.includes(" ")) {
+        // DEBUG
+        console.log("Invalid password: " + password);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Permet de check si pseudo rempli nos conditions fixées
+ *
+ * @param username
+ * @returns {boolean}
+ */
+function isUsernameValid(username) {
+    // Si notre pseudo
+    if (username.length < 3 || username.includes(" ")) {
+        // DEBUG
+        console.log("Invalid username: " + username);
+        return false;
+    }
+    return true;
+}
+
+// Dashboard de l'utilisateur
+router.get("/profile", (req, res) => {
+    // Si l'utilisateur est connecté
+    if(req.session.login){
+        // On fait le rendu de notre page en passant en paramètre les différentes informations du joueur
+        res.render(path.join(__dirname, "..", "..", "views", "profile"), {
+            username: req.session.username,
+        });
+    } else {
+        // On le redirige sur la page home
+        res.redirect("/")
+    }
+})
+
 // POST méthode pour enregistrer un compte
-router.post('/api/register', (req, res) => {
+router.post("/api/register", async (req, res) => {
     // On récupère les données de notre formulaire
     const username = req.body.username;
     const password = req.body.password;
-    console.log("post received: %s %s", username, password);
+    const confirm = req.body.passwordConfirm;
+
+    // On hash i.e "crypte" notre mot de passe
+    // Tant que l'exécution de la fonction generateHash n'est pas terminée on attend
+    const passwordHashed = await generateHash(req.body.password);
+
+    // Boolean qui vérifie si nos inputs correspondent avec nos conditions
+    // Cette variable va nous permettre aussi d'optimiser nos actions sur notre base de données
+    // on vient ainsi limiter l'utilisation des ressources serveur
+    let isInputValid = true;
+
+    // Nos deux mots de passes sont identiques
+    if (!isUsernameValid(username)) {
+        isInputValid = false;
+        req.flash("registerUsernameError", true);
+    }
+    if (!isPasswordValid(password)) {
+        isInputValid = false;
+        req.flash("registerPasswordError", true);
+    }
+    if (password !== confirm) {
+        isInputValid = false;
+        req.flash("registerConfirmError", true);
+    }
+
+    if (isInputValid) {
+        // On se connecte à notre base de données
+        pool.getConnection((error, connection) => {
+            if (error) throw error;
+            // On sélectionne dans notre base de données l'utilisateur ayant un username qui correspond à celui saisi sur le form
+            pool.query("SELECT Username FROM `users` WHERE username = ?", [username], (error, result) => {
+                // On affiche si une erreur est détectée
+                if(error) throw error;
+                // Si notre requête SQL aboutie alors elle admet forcément un résultat
+                // On check donc si un utilisateur n'est pas déjà enregistré avec ce pseudo
+                console.log(result)
+                // L'utilisateur existe déjà, on refresh la page pour afficher l'erreur
+                if(result.length > 0){
+                    // On coupe la connection à notre base de donnée
+                    connection.release();
+                    // Le pseudo est déjà utilisé on a donc une erreur qui va être traduite par une valeur false pour notre booléen
+                    isInputValid = false;
+                    // On update notre message flash qui sera affiché à l'utilisateur pour lui rendre compte de l'erreur
+                    req.flash("registerError", true);
+                    req.flash("registerUsernameAlreadyUsed", true)
+                    // On "reload" la page avec les flash messages (attention c'est une redirection qui est utilisée comme un reload)
+                    res.redirect("/");
+                } else {
+                    // Si on a survécu à nos différents tests ouffff...
+                    if (isInputValid === true) {
+                        // On va donc faire une dernière et ultime requête SQL afin de tout simplement ajouter notre utilisateur à notre base de donnée
+                        pool.query("INSERT INTO users (username, password) VALUES(?, ?)", [username, passwordHashed], (error) => {
+                            // On coupe la connection à notre base de donnée car on ne fais plus aucune requête
+                            connection.release();
+                            // On affiche si une erreur est détectée
+                            if (error) throw error;
+                            // On met à jour la session de l'utilisateur
+                            req.session.login = true;
+                            req.session.username = username;
+                            // On redirige notre utilisateur connecté vers son dashboard
+                            res.redirect("/profile");
+                        });
+                    }
+                }
+            })
+        })
+    } else {
+        req.flash("registerError", true);
+        res.redirect("/")
+    }
 })
+
+router.post("/api/login", (req, res) => {
+    // On récupère les données de notre formulaire
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // On lance une connexion à notre bdd
+    pool.getConnection((error, connection) => {
+        // On affiche l'erreur si le serveur n'arrive à se connecter à notre bdd
+        if(error) throw error;
+        // Requête SQL pour savoir si le pseudo et le mdp entré par l'utilisateur existe dans la bdd
+        pool.query("SELECT Username as username, Password as password FROM users WHERE username = ?", [username], async (error, result) => {
+            // On affiche l'erreur
+            if (error) throw error;
+            // On coupe la connexion à notre bdd
+            connection.release();
+            // Si un résultat est trouvé i.e. l'utilisateur existe
+            if (result.length > 0) {
+                // On créé une variable (boolean) qui va contenir le résultat de la fonction compareHash
+                // qui va nous permettre de savoir si le mot de passe saisi est bien celui présent
+                // dans notre base de donnée
+                let compare = await compareHash(password, result[0].password);
+                // Notre exécution de code s'arrête tant que compare na pas reçu le feu vert de la fonction compareHash
+                // On check si les mots passes correspondent
+                if(compare){
+                    // L'utilisateur existe + le mot de passe est valide on peut donc le connecter à son profil
+                    req.session.login = true;
+                    req.session.username = username;
+                    res.redirect("/profile");
+                } else {
+                    // On modifie notre flash message
+                    // On update notre message flash qui sera affiché à l'utilisateur pour lui rendre compte de l'erreur
+                    req.flash("loginError", true);
+                    req.flash("loginPasswordError", true)
+                    res.redirect("/");
+                }
+            } else {
+                // L'utilisateur n'existe pas
+                // On update notre message flash qui sera affiché à l'utilisateur pour lui rendre compte de l'erreur
+                req.flash("loginError", true);
+                req.flash("loginUsernameError", true)
+                res.redirect("/");
+            }
+        })
+    })
+})
+
+// Page pour se déconnecter (cette page n'existe pas réellement)
+router.get('/logout', function (req, res, next) {
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) return next(err);
+            else res.redirect('/');
+        });
+    }
+});
 
 module.exports = router;

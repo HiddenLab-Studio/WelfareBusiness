327487028c8c8dc16bc86e6b2adbef66b8b8b1c1
AurelienRoge <aurelien.roge@student.yncrea.fr>
2022-05-09 11:26:29 +0200

    debut classe projet

--- js/front/game/employee.js
+++ js/front/game/employee.js
@@ -2,9 +2,14 @@ class employee {
     constructor() {
         this.name = 'TBD';
         this.production = 1;
+        this.salary = 10;
     }
 
     getProduction() {
         return this.production;
+    }
+
+    getSalary(){
+        return this.salary;
     }
 }
--- js/front/game/welfareBusiness.js
+++ js/front/game/welfareBusiness.js
@@ -1,9 +1,36 @@
+//échelle de temps : 8s = 1j avec 1j -> 8h-20h (inutile de mettre la nuit)
+
 class welfareBusiness {
     constructor() {
         this.currentProject = new project(1000, 100)
         this.employeesList = new Array();
         this.employeesList[0] = new employee();
+        this.updateRate = 2 //update du jeu 2 fois par sec
     }
+
+    getTotalProduction() {
+        let production = 0;
+        for (let i = 0; i < this.employeesList.length; i++) {
+            production += this.employeesList[i].getProduction();
+        }
+
+        return production;
+    }
+
+    getTotalEmployeesCost(){
+        let cost = 0;
+        for(let i = 0; i < this.employeesList.length; i++){
+            cost = this.employeesList[i].getSalary();
+        }
+    }
+
+    convertToSec(seconds){
+        return seconds * this.updateProject;
+    }
+
+    
+
+
 
     updateProject() {
         for (let i = 0; i < this.employeesList.length; i++) {
@@ -13,12 +40,34 @@ class welfareBusiness {
         console.log(this.currentProject.getAmountToProduce());
     }
 
-    getCurrentProjectPercentage(){
+    getCurrentProjectPercentage() {
         return this.currentProject.getProjectPercentage();
     }
 
     addEmployee() {
         let newEmployee = new employee();
         employeesList.push(newEmployee);
+    }
+
+    generate3ProjectChoices() {
+        proposals = new Array(3);
+        proposal[0] = this.generateSafeProject();
+        proposal[1] = this.generateNullProject();
+        proposal[2] = this.generateAmbitiousProject();
+    }
+
+    generateSafeProject() {
+        let proposal = new project(this.getTotalProduction()*this.convertToSec(720), 1000/*AMODIFIER QUAND ON A LE SYSTEME DE TEMPS)*/);
+        return proposal;
+    }
+
+    generateNullProject() {
+        let proposal = new project(this.getTotalProduction()*this.convertToSec(720), 1000/*AMODIFIER QUAND ON A LE SYSTEME DE TEMPS)*/)
+        return proposal;
+    }
+
+    generateAmbitiousProject() {
+        let proposal = new project(this.getTotalProduction()*this.convertToSec(900), 1500/*AMODIFIER QUAND ON A LE SYSTEME DE TEMPS)*/)
+        return proposal;
     }
 }
